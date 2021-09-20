512e7ea737b030e850455f8d0481c935514d5e3c
tejaanuchuri0009@gmail.com <tejaanuchuri0009@gmail.com>
2021-09-21 02:00:07 +0530

    Add project files.

---
+++ WpfDataContextPropertyDemo/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDataContextPropertyDemo
+{
+    class Person
+    {
+        public string FirstName { set; get; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
